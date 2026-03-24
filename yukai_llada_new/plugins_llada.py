5b18d885720c57929e814057d9d93164d3a712d3
Yukai Jin <fish1928@outlook.com>
2026-03-24 14:36:25 -0400

    start to prepare cache version

--- yukai_llada_new/plugins_llada.py
+++ yukai_llada_new/plugins_llada.py
@@ -1,6 +1,9 @@
+import os
 import inspect
 from abc import ABC, abstractmethod
 
+import torch
+import torch.nn.functional as F
 
 class InspectorPlugin(ABC):
 
@@ -27,7 +30,7 @@ class InspectorPlugin(ABC):
         vars_caller = frame.f_locals
         self_caller = vars_caller['self']
         for k, v in kvargs.items():
-            self_caller[k] = v
+            setattr(self_caller, k, v)
         # end     
     # end
 
@@ -100,6 +103,14 @@ class SaveKVPreviousPlugin_Disabled(InspectorPlugin):
     def save(self):
         pass
     # end
+
+    def collect_kv_previous_and_calculate_sim_per_step_(self):
+        pass
+    # end
+
+    def aggregate_result(self):
+        pass
+    # end
 # end
 
 class SaveKVPreviousPlugin_Enabled(InspectorPlugin):
@@ -117,4 +128,157 @@ class SaveKVPreviousPlugin_Enabled(InspectorPlugin):
         self.save_vars(_k_previous=k, _v_previous=v)
     # end
 
+    '''aggregation and calculation'''
+
+    def _get_names_hidden(self):
+        return ['_k_previous','_v_previous']
+    # end
+
+    def __init__(self):
+        self.dict_hidden_to_matrixs_sim_per_step = {}
+        for name_hidden in self._get_names_hidden():
+            self.dict_hidden_to_matrixs_sim_per_step[name_hidden] = []
+        # end
+
+        self.dict_cache_kv_previous = {}        
+    # end
+
+    def collect_kv_previous_and_calculate_sim_per_step_(self):
+        id_batch, model, x = self.load_vars('id_batch', 'model', 'x')
+
+        dict_hidden_to_sims_layer = {}
+        for name_hidden in self._get_names_hidden():
+            dict_hidden_to_sims_layer[name_hidden] = []
+        # end
+
+        for block_transformer in model.model.transformer.blocks[:]:                       # take last all layers
+            id_block_transformer = block_transformer.layer_id
+            name_cache_base = f'batch_{id_batch}_layer_{id_block_transformer}'  # block and step in block
+
+            for name_hidden in self._get_names_hidden():
+                if hasattr(block_transformer, name_hidden):
+                    cache_current = getattr(block_transformer, name_hidden)
+                    name_cache = f'{name_cache_base}.{name_hidden}'
+
+                    if name_cache not in self.dict_cache_kv_previous:
+                        self.dict_cache_kv_previous[name_cache] = cache_current
+                        continue
+                    # end
+
+                    # we have current and last, calculate similarity
+                    cache_last = self.dict_cache_kv_previous[name_cache]
+                    self.dict_cache_kv_previous[name_cache] = cache_current  # udpate cache
+
+                    if cache_last.shape[1] < cache_current.shape[1]:
+                        cache_last = torch.cat([cache_last, cache_current[:, cache_last.shape[1]:, :]], dim=1)
+                    # end
+
+                    sim_neighbour = F.cosine_similarity(cache_current, cache_last, dim=-1).clamp(-1.0, 1.0)
+                    
+                    if sim_neighbour.shape[-1] < x.shape[-1]:
+                        sim_neighbour_padded = F.pad(
+                            sim_neighbour,
+                            (0, x.shape[-1]-sim_neighbour.shape[1]),
+                            value=1.0
+                        ).squeeze(0)
+                    else:
+                        sim_neighbour_padded = sim_neighbour.squeeze(0)
+                    # end
+
+                    dict_hidden_to_sims_layer[name_hidden].append(sim_neighbour_padded)
+                # end
+            # end
+        # end
+
+        for name_hidden in self._get_names_hidden():
+            sims_layer = dict_hidden_to_sims_layer[name_hidden]
+
+            if len(sims_layer) == 0:
+                break
+            # end
+            
+            matrix_sim_per_step = torch.stack(sims_layer, dim=0)
+            self.dict_hidden_to_matrixs_sim_per_step[name_hidden].append(matrix_sim_per_step)
+        # end
+    # end
+
+    def aggregate_result(self):
+        for name_hidden in self._get_names_hidden():
+            matrixs_sim_per_step = self.dict_hidden_to_matrixs_sim_per_step[name_hidden]
+            matrix_sim_per_step_layer_token = torch.stack(matrixs_sim_per_step, 0)  # dimension
+            return matrix_sim_per_step_layer_token.detach().float().cpu()
+        # end
+    # end
+
+    '''further calculation'''
+    def token_nonsimilarity_score_abs_per_step(
+        self,
+        sim: torch.Tensor,
+        p: float = 3.0,
+        type_fn: str = 'p'
+    ) -> torch.Tensor:
+
+        assert sim.ndim == 3, f"Expected 3D tensor [steps, layers, tokens], got shape {tuple(sim.shape)}"
+
+        diff = torch.abs(1.0 - sim)
+        if type_fn == 'p':
+            score = diff.pow(p).mean(dim=(1)).pow(1.0 / p)
+        elif type_fn == 'log':
+            score = torch.log1p(diff).mean(dim=(1))
+        # end
+
+        return score
+    # end
+
+    def load_sim_matrix_and_transform_to_most_diff_list(self, folder_kv_base, filename, num_block, len_prompt, size_block):
+        path_kv_file = os.path.join(folder_kv_base, filename)
+        matrix_sim_step_layer_token = torch.load(path_kv_file)
+        matrix_sim_step_layer_token = F.pad(matrix_sim_step_layer_token, (0,0,0,0,1,0), value=1.0)
+
+        list_idx_sim_sorted = []
+
+        for id_block in range(num_block):
+            position_start = len_prompt + size_block * id_block
+            matrix_sim_step_layer_token_cached = matrix_sim_step_layer_token[:size_block, :, :position_start]  #(steps_block, len_cached)
+            matrix_step_scores_diff_token = self.token_nonsimilarity_score_abs_per_step(matrix_sim_step_layer_token_cached)
+            matrix_step_idx_diff_token_decending = torch.argsort(matrix_step_scores_diff_token, dim=-1, descending=True)
+
+            for step in range(matrix_step_idx_diff_token_decending.shape[0]):
+                idxs_diff_token_decending = matrix_step_idx_diff_token_decending[step,:]
+
+                list_idx_sim_sorted.append({'filename': filename, 'block': id_block, 'step': step, 'idx': idxs_diff_token_decending.tolist(), 'value_raw': matrix_step_scores_diff_token[step,:].tolist()})
+            # end
+        # end
+
+        return list_idx_sim_sorted
+    # end
+
+    def dump_all_in_one(self):  # from test_get_top_change.ipynb
+        folder_kv_base = 'sims_kv_0315'
+        type_fn = 'p'
+        filename_report = f'all_in_one_sim_report_abs_per_step_{type_fn}_0315.json'
+
+        len_prompt = 512
+        num_block = 8
+        len_target = 1024
+        size_block = int(len_target / num_block)
+
+        dict_filename_to_list_idx_sorted = defaultdict(list)
+
+        for filename in os.listdir(folder_kv_base):
+            if filename[0] == '.':
+                continue
+            # end
+
+            # matrix_sim_step_layer_token, num_block, len_prompt, size_block, path_kv_base, filename
+            list_diff_sorted = self.load_sim_matrix_and_transform_to_most_diff_list(folder_kv_base, filename, num_block, len_prompt, size_block)
+            dict_filename_to_list_idx_sorted[filename] = list_diff_sorted
+        # end
+
+        with open(filename_report, 'w+') as file:
+            file.write(json.dumps(dict_filename_to_list_idx_sorted))
+        # end
+
+    # end
 # end
+
---
+++ yukai_llada_new/run_model_functions.py
@@ -0,0 +1,86 @@
+@ torch.no_grad()
+def run_model_semi(model, x, y, config_diffusion, *args):
+
+    '''declare required variables'''
+    num_blocks = config_diffusion.num_blocks
+    step_per_block = config_diffusion.step_per_block
+    size_block = config_diffusion.size_block
+    id_mask = config_diffusion.id_mask
+    len_prompt = config_diffusion.len_prompt
+    sorter = config_diffusion.klass_sorter()
+    collector = config_diffusion.klass_collector()
+    
+    p_finalized = torch.zeros(x.shape, dtype=torch.float64, device=x.device)
+    position_start = 0
+
+    for id_block in range(1, num_blocks+1):
+        position_end = position_start + len_prompt + id_block * size_block
+        mask_mask_blk = x[:,position_start:position_end] == id_mask
+
+        B = x.shape[0]
+        L = position_end - position_start
+        
+        idx_denoising = torch.arange(position_start, position_end, dtype=torch.long).to(x.device)
+        quota_helper = BlockDiffusionQuotaHelper(mask_mask_blk, size_block)
+
+        for step in range(step_per_block):
+            x_denoising,  y_denoising= x[:, idx_denoising], y[:, idx_denoising]
+            logits = model(x_denoising, idx_current=idx_denoising).logits
+            snapshot = SimpleLogitsSnapshot(logits, x_denoising, y_denoising, id_mask)
+            conf_snapshot = snapshot.transform_logits(collector)
+            idx_sorted_by_conf = sorter.argsort(conf_snapshot, snapshot)
+            num_unmask = quota_helper.get_quota(step)
+            idx_transform = idx_sorted_by_conf[:, :num_unmask]
+
+            snapshot.materialize_by_idx_(idx_transform, conf_snapshot)
+            snapshot.update_this(1, idx_transform, y=x).update_this(1, idx_transform, p_finalized=p_finalized)
+            
+        # end for step
+    # end for block
+
+    return p_finalized[:, len_prompt:]
+# end
+
+
+@ torch.no_grad()
+def run_model_semi_collect_kv(model, x, y, config_diffusion, id_batch=None, calculator_kvsim=None):
+
+    '''declare required variables'''
+    num_blocks = config_diffusion.num_blocks
+    step_per_block = config_diffusion.step_per_block
+    size_block = config_diffusion.size_block
+    id_mask = config_diffusion.id_mask
+    len_prompt = config_diffusion.len_prompt
+    sorter = config_diffusion.klass_sorter()
+    collector = config_diffusion.klass_collector()
+    
+    p_finalized = torch.zeros(x.shape, dtype=torch.float64, device=x.device)
+    position_start = 0
+
+    for id_block in range(1, num_blocks+1):
+        position_end = position_start + len_prompt + id_block * size_block
+        mask_mask_blk = x[:,position_start:position_end] == id_mask
+
+        B = x.shape[0]
+        L = position_end - position_start
+        
+        idx_denoising = torch.arange(position_start, position_end, dtype=torch.long).to(x.device)
+        quota_helper = BlockDiffusionQuotaHelper(mask_mask_blk, size_block)
+
+        for step in range(step_per_block):
+            x_denoising,  y_denoising= x[:, idx_denoising], y[:, idx_denoising]
+            logits = model(x_denoising, idx_current=idx_denoising).logits
+            snapshot = SimpleLogitsSnapshot(logits, x_denoising, y_denoising, id_mask)
+            conf_snapshot = snapshot.transform_logits(collector)
+            idx_sorted_by_conf = sorter.argsort(conf_snapshot, snapshot)
+            num_unmask = quota_helper.get_quota(step)
+            idx_transform = idx_sorted_by_conf[:, :num_unmask]
+
+            snapshot.materialize_by_idx_(idx_transform, conf_snapshot)
+            snapshot.update_this(1, idx_transform, y=x).update_this(1, idx_transform, p_finalized=p_finalized)
+            calculator_kvsim.collect_kv_previous_and_calculate_sim_per_step_()
+        # end for step
+    # end for block
+
+    return p_finalized[:, len_prompt:]
+# end
