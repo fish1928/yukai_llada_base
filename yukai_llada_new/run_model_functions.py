755106683495b052c831bd3acdad1182058ccd8b
Yukai Jin <fish1928@outlook.com>
2026-03-24 21:45:25 -0400

    in the middle, need to further modify tomorrow

--- yukai_llada_new/run_model_functions.py
+++ yukai_llada_new/run_model_functions.py
@@ -16,9 +16,6 @@ def run_model_semi(model, x, y, config_diffusion, *args, **kwargs):
     for id_block in range(num_blocks):
         position_end = position_start + len_prompt + (id_block+1) * size_block
         mask_mask_blk = x[:,position_start:position_end] == id_mask
-
-        B = x.shape[0]
-        L = position_end - position_start
         
         idx_denoising = torch.arange(position_start, position_end, dtype=torch.long).to(x.device)
         quota_helper = BlockDiffusionQuotaHelper(mask_mask_blk, size_block)
@@ -54,8 +51,10 @@ def run_model_semi_collect_kv(model, x, y, config_diffusion, *args, **kwargs):
     sorter = config_diffusion.klass_sorter()
     collector = config_diffusion.klass_collector()
 
+    '''accumulate only'''
     id_batch = kwargs['id_batch']
     calculator_kvsim = kwargs['calculator_kvsim']
+    '''accumulate only'''
     
     p_finalized = torch.zeros(x.shape, dtype=torch.float64, device=x.device)
     position_start = 0
@@ -64,9 +63,6 @@ def run_model_semi_collect_kv(model, x, y, config_diffusion, *args, **kwargs):
         position_end = position_start + len_prompt + (id_block+1) * size_block
         mask_mask_blk = x[:,position_start:position_end] == id_mask
 
-        B = x.shape[0]
-        L = position_end - position_start
-        
         idx_denoising = torch.arange(position_start, position_end, dtype=torch.long).to(x.device)
         quota_helper = BlockDiffusionQuotaHelper(mask_mask_blk, size_block)
 
@@ -81,7 +77,55 @@ def run_model_semi_collect_kv(model, x, y, config_diffusion, *args, **kwargs):
 
             snapshot.materialize_by_idx_(idx_transform, conf_snapshot)
             snapshot.update_this(1, idx_transform, y=x).update_this(1, idx_transform, p_finalized=p_finalized)
+
+            '''accumulate only'''
             calculator_kvsim.collect_kv_previous_and_calculate_sim_per_step_()  # DIFF: this line is the only difference
+            '''accumulate only'''
+        # end for step
+    # end for block
+
+    return p_finalized[:, len_prompt:]
+# end
+
+
+@ torch.no_grad()
+def run_model_semi_cached(model, x, y, config_diffusion, *args, **kwargs):
+
+    '''declare required variables'''
+    num_blocks = config_diffusion.num_blocks
+    step_per_block = config_diffusion.step_per_block
+    size_block = config_diffusion.size_block
+    id_mask = config_diffusion.id_mask
+    len_prompt = config_diffusion.len_prompt
+    sorter = config_diffusion.klass_sorter()
+    collector = config_diffusion.klass_collector()
+    
+    p_finalized = torch.zeros(x.shape, dtype=torch.float64, device=x.device)
+
+    idx_denoising = torch.arange(0, len_prompt, dtype=torch.long).to(x.device)
+    model(x[:, idx_denoising], idx_current=idx_denoising)   # save prompt for once
+
+    for id_block in range(num_blocks):
+        position_start = len_prompt + id_block * size_block
+        position_end = position_start + size_block
+        mask_mask_blk = x[:,position_start:position_end] == id_mask
+
+        idx_denoising = torch.arange(position_start, position_end, dtype=torch.long).to(x.device)
+        quota_helper = BlockDiffusionQuotaHelper(mask_mask_blk, size_block)
+
+        for step in range(step_per_block):
+            x_denoising,  y_denoising= x[:, idx_denoising], y[:, idx_denoising]
+            shape_target = (x.shape[0], position_end, -1)
+            logits = model(x_denoising, idx_current=idx_denoising, shape_target=shape_target).logits
+            snapshot = SimpleLogitsSnapshot(logits, x_denoising, y_denoising, id_mask)
+            
+            conf_snapshot = snapshot.transform_logits(collector)
+            idx_sorted_by_conf = sorter.argsort(conf_snapshot, snapshot)
+            num_unmask = quota_helper.get_quota(step)
+            idx_transform = idx_sorted_by_conf[:, :num_unmask]
+
+            snapshot.materialize_by_idx_(idx_transform, conf_snapshot)
+            snapshot.update_this(1, idx_src=idx_transform, idx_tgt=idx_denoising, y=x).update_this(1, idx_src=idx_transform, idx_tgt=idx_denoising, p_finalized=p_finalized)
         # end for step
     # end for block
 
