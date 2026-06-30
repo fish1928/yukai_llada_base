f7fb1d43756ae86c0970a2f044e6a7cf7d6930d5
Yukai Jin <fish1928@outlook.com>
2026-06-30 18:40:07 -0400

    update?

--- yukai_llada_benchmark/constants_llada.py
+++ yukai_llada_benchmark/constants_llada.py
@@ -3,7 +3,7 @@ import torch
 DTYPE_EVAL = torch.bfloat16
 TEXT_MASK = '<|mdm_mask|>'
 ID_MASK = 126336
-NAME_MLP = 'mlp_attn_ifeval_64.pt'
-# NAME_MLP = 'mlp_attn_gsm8k_64.pt'   # 0.125 on instruction in ifeval
+# NAME_MLP = 'mlp_attn_ifeval_64.pt'
+NAME_MLP = 'mlp_attn_gsm8k_64.pt'   # 0.125 on instruction in ifeval
 # NAME_MLP = 'mlp_attn.pt'  # -> 0000 on ifeval
 NAME_MLP3 = 'mlp_3_gsm8k_64.pt'
