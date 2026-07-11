3c2084341a44969d3154d3b0b507fdae10b18745
Yukai Jin <fish1928@outlook.com>
2026-07-11 13:10:05 -0400

    start to work today

--- yukai_llada_olds/yukai_llada_benchmark/run.sh
+++ yukai_llada_olds/yukai_llada_benchmark/run.sh
@@ -13,5 +13,5 @@ accelerate launch --num_processes=1 run_benchmark_main.py --tasks gsm8k --limit 
 # run llada semi log
 accelerate launch --num_processes=1 run_benchmark_main.py --tasks gsm8k --limit 1 --model test --batch_size 1 --num_fewshot 5 --model_args id_model='GSAI-ML/LLaDA-8B-Base',size_batch=1,len_target=128,num_blocks=2,num_unmask_per_step=1,id_mask=126336,device='cuda:0',runner=run_model_semi_with_log
 
-
+#
 accelerate launch --num_processes=1 run_benchmark_main.py --tasks gsm8k --limit 5 --model test --batch_size 1 --num_fewshot 5 --model_args id_model='GSAI-ML/LLaDA-8B-Base',size_batch=1,len_target=128,num_blocks=2,num_unmask_per_step=1,id_mask=126336,device='cuda:0',step_refresh_remainder=2,select_only_in_h=True,runner=run_model_semi_cached_mlp3
